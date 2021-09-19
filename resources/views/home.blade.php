5ed9d8595c050bbe6660cb0fd28c0b8f06f9b2ff
frchandra <nismara.chandra@gmail.com>
2021-09-19 12:39:20 +0700

    test repo di tempat hosting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <!-- Masthead-->
     <header class="masthead">
         <div class="container">
-            <div class="masthead-subheading">Welcome To Our Studio!</div>
+            <div class="masthead-subheading">Welcome To Our Website!</div>
             <div class="masthead-heading text-uppercase">It's Nice To Meet You</div>
 
             <table class="table table-light table-striped">
